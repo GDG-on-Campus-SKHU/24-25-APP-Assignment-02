85d274c4d58378d0c971a964c309b1cd8e8b78f2
lee, arim <arim0930@gmail.com>
2024-10-05 23:29:50 +0900

    message

--- 이아림/hw4.dart
+++ 이아림/hw4.dart
@@ -13,3 +13,25 @@
     - 강의자료 Enum 부분처럼 해당 Enum의 값을 저장하는 변수 선언해서 날씨 상태 설정
     - break 사용하기.
   */ // Weather Enum 정의
+
+enum Weather{
+    sunny, rainy, cloudy, snowy
+  }
+void main() {
+  Weather today = Weather.snowy;
+
+  //
+  switch(today)
+  {
+    case Weather.sunny:
+      print('오늘은 맑은 날씨입니다!');
+    case Weather.rainy:
+      print('오늘은 비가 오는 날씨입니다!');
+    case Weather.cloudy:
+      print('오늘은 흐린 날씨입니다!');
+    case Weather.snowy:
+      print('오늘은 눈이 오는 날씨입니다!');
+  }
+
+
+}
